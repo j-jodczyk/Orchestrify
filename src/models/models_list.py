36b0d36b25193f58acc6c5d3ae6a5a4960539b3e
Milos-Ony <01159110@pw.edu.pl>
2025-01-12 21:22:33 +0100

    update models_list.py

--- src/models/models_list.py
+++ src/models/models_list.py
@@ -1,10 +1,38 @@
 models = {
     "Lakh": {
         "model": "rasta3050/aiguru_lakh",
-        "tokenizer": "Milos121/cleaner_lakh_MMM",
+        "tokenizer": "rasta3050/aiguru_lakh",
     },
     "Lakh Game Themes": {
-        "model": "Milos121/transfer_learning_clean_lakh_game_themes",
-        "tokenizer": "Milos121/transfer_learning_clean_lakh_game_themes",
+        "model": "rasta3050/lakh_game_themes_transfer_model",
+        "tokenizer": "rasta3050/lakh_game_themes_transfer_model",
+    },
+    "Lakh Rock": {
+        "model": "rasta3050/lakh_rock_transfer_model",
+        "tokenizer": "rasta3050/lakh_rock_transfer_model",
+    },
+    "Lakh Pop": {
+        "model": "rasta3050/lahk_pop_transfer_model",
+        "tokenizer": "rasta3050/lahk_pop_transfer_model",
+    },
+    "Lakh Jazz": {
+        "model": "rasta3050/lakh_jazz_transfer_model",
+        "tokenizer": "rasta3050/lakh_jazz_transfer_model",
+    },
+    "Game Themes": {
+        "model": "rasta3050/aiguru_game_themes",
+        "tokenizer": "rasta3050/aiguru_game_themes",
+    },
+    "Rock": {
+        "model": "rasta3050/aiguru_rock",
+        "tokenizer": "rasta3050/aiguru_rock",
+    },
+    "Pop": {
+        "model": "rasta3050/aiguru_pop",
+        "tokenizer": "rasta3050/aiguru_pop",
+    },
+    "Jazz": {
+        "model": "rasta3050/aiguru_jazz",
+        "tokenizer": "rasta3050/aiguru_jazz",
     },
 }
